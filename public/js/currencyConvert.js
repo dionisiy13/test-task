fed413e98f00d976869a7349f39140afa2640370
Dan Kurasov <dionisiy13@gmail.com>
2020-02-13 09:48:55 +0200

    Convert negative numbers to positive

--- public/js/currencyConvert.js
+++ public/js/currencyConvert.js
@@ -12,6 +12,11 @@ $(document).ready(function() {
         let currentValue = $("[name='value']").val();
         let currency = $("[name='currency']").val();
 
+        if (currentValue < 0) {
+            currentValue *= -1;
+            $("[name='value']").val(currentValue);
+        }
+
         let result = parseFloat(rates[currency] * currentValue).toFixed(2);
 
         $("#result").html(result);
